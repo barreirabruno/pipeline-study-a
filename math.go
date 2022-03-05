f9031163c0ca8cf6f9e48c35d8b3971416b5f87d
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-05 15:37:49 -0300

    refactor: update function name

--- math.go
+++ math.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(10, 10))
-	fmt.Println(greeting("John"))
+	fmt.Println(Greeting("John"))
 }
 
 func soma(a int, b int) int {
 	return a + b
 }
 
-func greeting(name string) string {
+func Greeting(name string) string {
 	greeting := "Hello " + name
 	return greeting
 }
