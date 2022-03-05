806c193043a87e79fc339f39ea3957610d0ef8c6
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-05 15:03:44 -0300

    feat: add greeting function

--- math.go
+++ math.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(10, 10))
+	fmt.Println(greeting("John"))
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func greeting(name string) string {
+	greeting := "Hello " + name
+	return greeting
 }
